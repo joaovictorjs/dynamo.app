93167a077ab0000279fd46bccfe93d06e061a0fd
joaovictorjs <skai7ami@gmail.com>
2025-04-07 20:18:47 -0300

    dummy data

---
+++ dummy.go
@@ -0,0 +1,19 @@
+package main
+
+var dummyReportNames = &[]string{
+	"Relatório Financeiro Mensal",
+	"Análise de Desempenho Corporativo",
+	"Demonstrativo de Resultados",
+	"Relatório de Indicadores Econômicos",
+	"Balanço Patrimonial Consolidado",
+	"Sumário Executivo Financeiro",
+	"Avaliação de Riscos e Oportunidades",
+	"Relatório de Gestão Orçamentária",
+	"Panorama Econômico Trimestral",
+	"Estudo de Viabilidade Financeira",
+	"Relatório de Fluxo de Caixa",
+	"Análise de Rentabilidade Operacional",
+	"Relatório de Projeções Financeiras",
+	"Síntese de Resultados Empresariais",
+	"Demonstrativo de Ativos e Passivos",
+}
