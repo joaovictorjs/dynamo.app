da149a0e42eced9f9388bd0f0e6b2fc069b69d11
joaovictorjs <skai7ami@gmail.com>
2025-04-07 19:08:18 -0300

    create dynamo theme

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	app := app.New()
+	app.Settings().SetTheme(newDynamoTheme())
 	win := app.NewWindow("dynamo.app")
 
 	win.SetPadded(false)
 	win.CenterOnScreen()
-	win.SetContent(widget.NewLabel("Hello World!"))
+	win.SetContent(container.NewVBox(widget.NewLabel("Hello World!"), widget.NewSeparator()))
 	win.ShowAndRun()
 }
