3322a53107baea65b417d310c986d0359c6a6008
jotave <skai7ami@gmail.com>
2025-04-09 13:06:56 -0300

    create custom theme

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fyne.io/fyne/v2/app"
 
 func main() {
 	application := app.New()
+	application.Settings().SetTheme(newDynamoTheme())
 	mainWindow := application.NewWindow("dynamo.app")
 
 	gui := newGui(mainWindow)
---
+++ theme.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
+)
+
+type dynamoTheme struct {
+	fyne.Theme
+}
+
+func newDynamoTheme() *dynamoTheme {
+	return &dynamoTheme{Theme: theme.DefaultTheme()}
+}
+
+func (t *dynamoTheme) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameSeparator {
+		return color.RGBA{54, 54, 54, 255}
+	}
+
+	return t.Theme.Color(name, theme.VariantDark)
+}
+
+func (t *dynamoTheme) Font(name fyne.TextStyle) fyne.Resource {
+	return t.Theme.Font(name)
+}
+
+func (t *dynamoTheme) Icon(name fyne.ThemeIconName) fyne.Resource {
+	return t.Theme.Icon(name)
+}
+
+func (t *dynamoTheme) Size(name fyne.ThemeSizeName) float32 {
+	if name == theme.SizeNameText {
+		return 12
+	}
+
+	return t.Theme.Size(name)
+}
